daf40398d686837db14db996f227305be8303fff
Jacob Cassady <jtcass01@louisville.edu>
2022-12-01 00:00:01 -0500

    Changing template layout

---
+++ 2022/Day x/src/solution.cpp
@@ -0,0 +1,23 @@
+#include "solution.hpp"
+
+std::string loadFile(std::string fileName) {
+    std::ifstream File(fileName, std::fstream::in);
+    std::string str;
+
+    // Alocate memory for file.
+    File.seekg(0, std::ios::end);
+    str.reserve(File.tellg());
+    File.seekg(0, std::ios::beg);
+
+    // Store file data inside str
+    str.assign((std::istreambuf_iterator<char>(File)),
+                std::istreambuf_iterator<char>());
+
+    return str;
+}
+
+int main() {
+  std::cout << "example.txt:" << std::endl << loadFile("example.txt") << std::endl;
+  std::cout << "input.txt:" << std::endl << loadFile("input.txt") << std::endl;
+  return 0;
+}
