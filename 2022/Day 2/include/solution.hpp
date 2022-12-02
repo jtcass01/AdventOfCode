68ef993315c4dbfe22dddc493eb6b0b2de4f2d38
Jacob Cassady <jtcass01@louisville.edu>
2022-12-02 00:45:34 -0500

    Forgot to move enum before class

--- 2022/Day 2/include/solution.hpp
+++ 2022/Day 2/include/solution.hpp
@@ -5,29 +5,6 @@
 #include <cassert>
 #include <stdio.h>
 
-class RockPaperScissors {
-    public:
-        RockPaperScissors();
-
-        void addRound(Move myMove, Move opMove);
-
-        int getMyScore();
-
-    private:
-        Result decideRound(Move myMove, Move opMove);
-
-        unsigned int myScore_;
-        unsigned int opScore_;
-};
-
-Move classifyMove(MyMove myMove);
-
-Move classifyMove(OpMove opMove);
-
-int partOne(const std::string fileName);
-
-int partTwo(const std::string fileName);
-
 enum Move {
     rock = 1,
     paper = 2,
@@ -51,3 +28,26 @@ enum OpMove {
     OM_paper = 'B',
     OM_scissors = 'C'
 };
+
+class RockPaperScissors {
+    public:
+        RockPaperScissors();
+
+        void addRound(Move myMove, Move opMove);
+
+        int getMyScore();
+
+    private:
+        Result decideRound(Move myMove, Move opMove);
+
+        unsigned int myScore_;
+        unsigned int opScore_;
+};
+
+Move classifyMove(MyMove myMove);
+
+Move classifyMove(OpMove opMove);
+
+int partOne(const std::string fileName);
+
+int partTwo(const std::string fileName);
