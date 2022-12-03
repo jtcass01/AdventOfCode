871cf8d56c2120add0d94c7abbed7bb6dbc5d485
Jacob Cassady <jtcass01@louisville.edu>
2022-12-03 00:57:06 -0500

    Was missing the last group

--- 2022/Day 3/src/solution.cpp
+++ 2022/Day 3/src/solution.cpp
@@ -75,7 +75,7 @@ int partTwo(const std::string fileName) {
 
   for(std::string line; std::getline(File, line);) {
     std::cout << "Line " << line << " with ruckSacks.size() == " << ruckSacks.size() << std::endl;
-    if(ruckSacks.size() < 3) {
+    if(ruckSacks.size() < 2) {
       ruckSacks.push_back(line);
     } else {
       RucksackGroup ruckSackGroup(ruckSacks);
