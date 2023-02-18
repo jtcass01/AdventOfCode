4234965e2c981d3c3c89d688855a70460e10d42b
Jacob Cassady <jacobtaylorcassady@outlook.com>
2023-02-18 14:43:25 -0500

    Day 5 part 1

--- 2019/cpp/Day 5/include/computer.hpp
+++ 2019/cpp/Day 5/include/computer.hpp
@@ -13,70 +13,29 @@
 #include <sstream>
 #include <exception>
 
-template <typename T>
-T sum(std::vector<T> values);
+#include "../include/utils.hpp"
+#include "../include/mode.hpp"
+#include "../include/opcode.hpp"
+#include "../include/instruction.hpp"
 
-template <typename T>
-T product(std::vector<T> values);
-
-enum OPCODE {
-    FINISHED = 99,
-    ADD = 1,
-    MULTIPLY = 2,
-    WRITE = 3,
-    READ = 4,
-    ERROR = 0
-};
-
-std::ostream &operator<<(std::ostream &os, const OPCODE opcode);
-
-std::string to_string(const OPCODE &opcode);
-
-OPCODE getOpcode(int registerValue);
-
-unsigned int getInstructionSize(const OPCODE opcode);
-
-unsigned int getParameterCount(const OPCODE opcode);
-
-enum MODE {
-    POSITION = 0,
-    IMMEDIATE = 1
-};
-
-std::ostream &operator<<(std::ostream &os, const MODE mode);
-
-std::string to_string(const MODE &mode);
-
-MODE getMode(int registerValue);
-
-std::vector<MODE> getParameterModes(const std::vector<signed int> instruction,
-                                    const OPCODE opcode);
 
 class Computer {
     public:
         Computer(const std::string programFile);
         Computer(std::vector<signed int> registers);
 
-        // void setupInstruction(std::vector<signed int> *pInstruction);
-
-        std::vector<signed int> getInstruction(std::vector<signed int>::iterator *instructionStart);
-
         void write(const unsigned int registerNumber, const signed int registerValue);
-
-        void startUp(void);
 
         const signed int read(const unsigned int registerNumber) const;
 
-        const std::vector<signed int> getRegisters() const;
+        void startUp(void);
 
-        const std::vector<signed int> getParameterValues(const std::vector<signed int> instruction,
-                                                         const std::vector<MODE> parameterModes,
-                                                         const OPCODE opcode);
+        const std::vector<signed int> getRegisters() const;
 
     private:
         std::vector<signed int> registers_;
 
-        OPCODE injestIntcode(const std::vector<signed int> instruction);
+        OPCODE injestIntcode(const Instruction instruction);
 };
 
 std::ostream &operator<<(std::ostream &os, const Computer computer);
--- 2019/cpp/Day 5/src/computer.cpp
+++ 2019/cpp/Day 5/src/computer.cpp
@@ -1,254 +1,5 @@
 #include "../include/computer.hpp"
 
-
-template <typename T>
-T sum(const std::vector<T> values) {
-    T result = 0;
-
-    for (T value : values) {
-        result += value;
-    }
-
-    return result;
-}
-
-template <typename T>
-T product(const std::vector<T> values) {
-    T result = 1;
-
-    for (T value : values) {
-        result *= value;
-    }
-
-    return result;
-}
-
-std::ostream &operator<<(std::ostream &os, const OPCODE opcode) {
-    os << to_string(opcode);
-    return os;
-}
-
-std::string to_string(const OPCODE &opcode) {
-    std::stringstream errorMessage;
-    std::string result = "";
-
-    switch(opcode) {
-        case OPCODE::FINISHED:
-            result = "finished";
-            break;
-        case OPCODE::ADD:
-            result = "add";
-            break;
-        case OPCODE::MULTIPLY:
-            result = "multiply";
-            break;
-        case OPCODE::WRITE:
-            result = "write";
-            break;
-        case OPCODE::READ:
-            result = "read";
-            break;
-        case OPCODE::ERROR:
-            result = "error";
-            break;
-        default:
-            errorMessage << "to_string(const OPCODE &opcode) not implemented for given OPCODE:" << opcode << std::endl;
-            throw std::runtime_error(errorMessage.str());
-    }
-
-    return result;
-}
-
-OPCODE getOpcode(int registerValue) {
-    assert(registerValue > 0);
-    OPCODE opcode = OPCODE::ERROR;
-
-    std::cout << "Getting op code for value: " << registerValue << std::endl;
-
-    if (registerValue < 100) {
-        opcode = static_cast<OPCODE>(registerValue);
-    } else {
-        std::string stringReprestation = std::to_string(registerValue);
-        int opValue = stoi(stringReprestation.substr(stringReprestation.length() - 2));
-        opcode = static_cast<OPCODE>(opValue);
-    }
-
-    return opcode;
-};
-
-std::ostream &operator<<(std::ostream &os, const MODE mode) {
-    os << to_string(mode);
-    return os;
-}
-
-std::string to_string(const MODE &mode) {
-    std::stringstream errorMessage;
-    std::string result = "";
-
-    switch(mode) {
-        case MODE::POSITION:
-            result = "position";
-            break;
-        case MODE::IMMEDIATE:
-            result = "immediate";
-            break;
-        default:
-            errorMessage << "to_string(const MODE &mode) not implemented for given MODE: " << mode << std::endl;
-            throw std::runtime_error(errorMessage.str());
-    }
-
-    return result;
-}
-
-MODE getMode(int registerValue) {
-    assert(registerValue > 0);
-
-    MODE mode = MODE::POSITION;
-
-    if (registerValue > 99) {
-        mode = MODE::IMMEDIATE;
-    }
-
-    return mode;
-}
-
-std::vector<MODE> getParameterModes(const std::vector<signed int> instruction,
-                                    const OPCODE opcode) {
-    std::string strOp = std::to_string(instruction[0]);
-    unsigned int instructionSize = getInstructionSize(opcode);
-    std::vector<MODE> parameterModes(instructionSize, MODE::POSITION);
-
-    if (instruction[0] > 99) {
-        for(unsigned int parameterReverseIndex = strOp.size() - 2 - 1,
-            parameterIndex = 0;
-            parameterIndex >= 0;
-            parameterReverseIndex--, parameterIndex++) {
-            if (strOp[parameterReverseIndex] == '1') {
-                parameterModes[parameterIndex] = MODE::IMMEDIATE;
-            }
-        }
-    }
-
-    std::cout << "Parameter modes for " << instruction << " are ";
-
-    std::cout << "[";
-    // Loop through the vector and print each element
-    for(MODE &mode : parameterModes) {
-
-    }
-    for (unsigned int vector_i = 0; vector_i < parameterModes.size(); vector_i++) {
-        std::cout << parameterModes[vector_i];
-
-        // Add a comma after each element, except the last one
-        if (vector_i != parameterModes.size() - 1) {
-            std::cout << ", ";
-        }
-    }
-    std::cout << "]" << std::endl;
-
-    return parameterModes;
-}
-
-
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<T> vec) {
-    os << "[";
-    // Loop through the vector and print each element
-    for (unsigned int vector_i = 0; vector_i < vec.size(); vector_i++) {
-        os << vec[vector_i];
-
-        // Add a comma after each element, except the last one
-        if (vector_i != vec.size() - 1) {
-            os << ", ";
-        }
-    }
-    os << "]";
-
-    return os;
-}
-
-unsigned int getInstructionSize(const OPCODE opcode) {
-    std::stringstream errorMessage;
-    int instructionSize = 0;
-
-    switch(opcode) {
-    case OPCODE::FINISHED:
-        instructionSize = 1;
-        break;
-    case OPCODE::ADD:
-        instructionSize = 4;
-        break;
-    case OPCODE::MULTIPLY:
-        instructionSize = 4;
-        break;
-    case OPCODE::WRITE:
-        instructionSize = 2;
-        break;
-    case OPCODE::READ:
-        instructionSize = 2;
-        break;
-    case OPCODE::ERROR:
-    default:
-        instructionSize = 0;
-        errorMessage << "getInstructionSize(...) not implemented for given OPCODE: " << opcode << std::endl;
-        throw std::runtime_error(errorMessage.str());
-    }
-
-    return instructionSize;
-}
-
-unsigned int getParameterCount(const OPCODE opcode) {
-    std::stringstream errorMessage;
-    int parameterSize = 0;
-
-    switch(opcode) {
-    case OPCODE::FINISHED:
-        parameterSize = 0;
-        break;
-    case OPCODE::ADD:
-        parameterSize = 2;
-        break;
-    case OPCODE::MULTIPLY:
-        parameterSize = 2;
-        break;
-    case OPCODE::WRITE:
-        parameterSize = 1;
-        break;
-    case OPCODE::READ:
-        parameterSize = 1;
-        break;
-    case OPCODE::ERROR:
-    default:
-        parameterSize = 0;
-        errorMessage << "getInstructionSize(...) not implemented for given OPCODE: " << opcode << std::endl;
-        throw std::runtime_error(errorMessage.str());
-    }
-
-    return parameterSize;
-}
-
-const std::vector<signed int> Computer::getParameterValues(
-    const std::vector<signed int> instruction,
-    const std::vector<MODE> parameterModes,
-    const OPCODE opcode) {
-
-    std::vector<signed int> parameterValues;
-
-    unsigned int parameterCount = getParameterCount(opcode);
-    for(unsigned int parameterValueIndex = 0; parameterValueIndex < parameterCount; parameterValueIndex++) {
-        switch(parameterModes[1+parameterValueIndex]) {
-            case MODE::IMMEDIATE:
-                parameterValues.push_back(instruction[1+parameterValueIndex]);
-                break;
-            case MODE::POSITION:
-                parameterValues.push_back(read(instruction[1+parameterValueIndex]));
-                break;
-        }
-    }
-
-    return parameterValues;
-}
-
 Computer::Computer(const std::string programFileName) {
   std::string programText;
   std::ifstream programFile;
@@ -267,53 +18,31 @@ Computer::Computer(const std::string programFileName) {
 
 Computer::Computer(std::vector<signed int> registers) : registers_(registers) {}
 
-std::vector<signed int> Computer::getInstruction(std::vector<signed int>::iterator *instructionStart) {
-    unsigned int startIndex = std::distance(registers_.begin(), *instructionStart);
-    OPCODE opcode = getOpcode(registers_[startIndex]);
-    std::cout << "opcode: " << opcode << std::endl;
-    unsigned int instructionSize = getInstructionSize(opcode);
-    std::cout << "instructionSize: " << instructionSize << std::endl;
-    std::vector<signed int> instruction(*instructionStart, *instructionStart+instructionSize);
-    std::cout << "instruction: " << instruction << std::endl;
-    *instructionStart += instructionSize;
-    return instruction;
-}
-
-OPCODE Computer::injestIntcode(const std::vector<signed int> instruction) {
+OPCODE Computer::injestIntcode(const Instruction instruction) {
     std::stringstream errorMessage;
     assert(instruction.size() > 0);
-
-    OPCODE opcode = static_cast<OPCODE>(instruction[0]);
-    std::vector<MODE> parameterModes = getParameterModes(instruction,
-                                                         opcode);
-    unsigned int expectedIntructionSize =  getInstructionSize(opcode);
     signed int result = 0;
-    assert(instruction.size() == expectedIntructionSize);
 
-    std::vector<signed int> parameterValues = getParameterValues(instruction,
-                                                                 parameterModes,
-                                                                 opcode);
-
-    switch(opcode) {
+    switch(instruction.getOpcode()) {
         case OPCODE::FINISHED:
             std::cout << "STOP OPCODE RECEIVED." << std::endl;
             break;
         case OPCODE::ADD:
-            result = sum<signed int>(parameterValues);
-            write(instruction[3], result);
+            result = sum<signed int>(instruction.getParameters());
+            write(instruction.getDestination(), result);
             break;
         case OPCODE::MULTIPLY:
-            result = product<signed int>(parameterValues);
-            write(instruction[3], result);
+            result = product<signed int>(instruction.getParameters());
+            write(instruction.getDestination(), result);
             break;
         case OPCODE::WRITE:
-            std::cout << std::to_string(instruction[1]) << " input: ";
+            std::cout << std::to_string(instruction.getParameter(0)) << " input: ";
             std::cin >> result;
-            write(instruction[1], result);
+            write(instruction.getParameter(0), result);
             break;
         case OPCODE::READ:
-            result = read(instruction[1]);
-            std::cout << std::to_string(instruction[1]) << " output: ";
+            result = read(instruction.getParameter(0));
+            std::cout << std::to_string(instruction.getParameter(0)) << " output: ";
             std::cout << result << std::endl;
             break;
         case OPCODE::ERROR:
@@ -322,7 +51,15 @@ OPCODE Computer::injestIntcode(const std::vector<signed int> instruction) {
             throw std::runtime_error(errorMessage.str());
     }
 
-    return opcode;
+    return instruction.getOpcode();
+}
+
+void Computer::write(const unsigned int registerNumber, const signed int registerValue) {
+    registers_[registerNumber] = registerValue;
+}
+
+const signed int Computer::read(const unsigned int registerNumber) const {
+  return registers_[registerNumber];
 }
 
 void Computer::startUp(void) {
@@ -332,24 +69,20 @@ void Computer::startUp(void) {
 
     while(instructionStart < registers_.end()
        && opcode != OPCODE::FINISHED) {
-        std::vector<signed int> instruction = getInstruction(&instructionStart);
+        Instruction instruction = Instruction(&instructionStart,
+                                              getRegisters());
+
         std::cout << "instruction: " << instruction << std::endl;
         std::cout << "\tbefore registers_ state: " << getRegisters() << std::endl;
         opcode = injestIntcode(instruction);
-        std::cout << "\tafter instruction: " << instruction << std::endl;
-    }
-}
+        std::cout << "\tafter registers_ state: " << getRegisters() << std::endl;
 
-void Computer::write(const unsigned int registerNumber, const signed int registerValue) {
-    registers_[registerNumber] = registerValue;
+        *instructionStart += instruction.size();
+    }
 }
 
 const std::vector<signed int> Computer::getRegisters() const {
     return registers_;
-}
-
-const signed int Computer::read(const unsigned int registerNumber) const {
-  return registers_[registerNumber];
 }
 
 std::ostream &operator<<(std::ostream &os, const Computer computer) {
