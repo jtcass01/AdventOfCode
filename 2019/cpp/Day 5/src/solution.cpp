81b016434609aea21b5d3da12ccafdf0f38071b3
Jacob Cassady <jacobtaylorcassady@outlook.com>
2023-02-19 14:29:25 -0500

    Added more tests to day 5 part 2

--- 2019/cpp/Day 5/src/solution.cpp
+++ 2019/cpp/Day 5/src/solution.cpp
@@ -35,6 +35,44 @@ int main() {
   std::cout << "Part One Input Result: " << partOneResult << std::endl;
   assert(3 == partOneResult);
 
+  /// EXAMPLE 0 PART 2
+  std::cout << "Testing equal to in position mode. Input an 8... " << std::endl;
+  std::vector<signed int> exampleEqualPosition = {3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8};
+  Computer testComputer(exampleEqualPosition);
+  testComputer.startUp();
+  std::cout << "If you saw a 1, the test was a success." << std::endl;
+
+  /// EXAMPLE 1 PART 2
+  std::cout << "Testing less than in position mode. Input a value less than 8... " << std::endl;
+  std::vector<signed int> exampleLessThanPosition = {3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8};
+  Computer testComputer(exampleLessThanPosition);
+  testComputer.startUp();
+  std::cout << "If you saw a 1, the test was a success." << std::endl;
+
+  /// EXAMPLE 2 PART 2
+  std::cout << "Testing equal to in immediate mode. Input an 8... " << std::endl;
+  std::vector<signed int> exampleEqualImmediate = {3, 3, 1108, -1, 8, 3, 4, 3, 99};
+  Computer testComputer(exampleEqualImmediate);
+  testComputer.startUp();
+  std::cout << "If you saw a 1, the test was a success." << std::endl;
+
+  /// EXAMPLE 3 PART 2
+  std::cout << "Testing less than in immediate mode. Input a value less than 8... " << std::endl;
+  std::vector<signed int> exampleLessThanImmediate = {3, 3, 1107, -1, 8, 3, 4, 3, 99};
+  Computer testComputer(exampleLessThanImmediate);
+  testComputer.startUp();
+  std::cout << "If you saw a 1, the test was a success." << std::endl;
+
+  std::cout << "Testing jump in position mode. Expect a 1 if the value is nonzero or a 0 if the value is 0..." << std::endl;
+  std::vector<signed int> exampleJumpPosition = {3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9};
+  Computer testComputer(exampleJumpPosition);
+  testComputer.startUp();
+
+  std::cout << "Testing jump in immediate mode. Expect a 1 if the value is nonzero or a 0 if the value is 0..." << std::endl;
+  std::vector<signed int> exampleJumpImmediate = {3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1};
+  Computer testComputer(exampleJumpImmediate);
+  testComputer.startUp();
+
   int partTwoResult = partTwo("input.txt");
   std::cout << "Part Two Input Result: " << partTwoResult << std::endl;
   assert(0 == partTwoResult);
