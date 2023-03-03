f211305fa9e4ad13b6a97f5848f31e5c0a0bb93f
Jacob Cassady <jacobtaylorcassady@outlook.com>
2023-03-03 17:48:25 -0500

    Added unittests for day 2

--- 2019/cpp/Day 5/src/unittest.cpp
+++ 2019/cpp/Day 5/src/unittest.cpp
@@ -32,17 +32,6 @@ TEST(Day2Test, StopCode) {
               true);
 }
 
-
-TEST(Day2Test, StopCode) {
-    std::vector<signed int> exampleRegisters = {2, 4, 4, 5, 99, 0};
-    const std::vector<signed int> expectedResult = {2, 4, 4, 5, 99, 9801};
-
-    Computer testComputer(exampleRegisters);
-    testComputer.startUp();
-    EXPECT_EQ(sameVectors(testComputer.getRegisters(), expectedResult),
-              true);
-}
-
 TEST(Day2Test, LongBoi) {
     std::vector<signed int> exampleRegisters =     {1,  1, 1, 4, 99, 5, 6, 0, 99};
     const std::vector<signed int> expectedResult = {30, 1, 1, 4, 2,  5, 6, 0, 99};
