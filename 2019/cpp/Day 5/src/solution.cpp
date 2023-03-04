fa003fcd23a8eb7df7ee95d6871852936650ba2b
Jacob Cassady <jacobtaylorcassady@outlook.com>
2023-03-04 00:56:37 -0500

    Working unittests from Day 4. Adding verbose flag to computer.

--- 2019/cpp/Day 5/src/solution.cpp
+++ 2019/cpp/Day 5/src/solution.cpp
@@ -3,7 +3,7 @@
 int partOne(const std::string fileName) {
   std::cout << "Part 1: " << fileName << std::endl;
 
-  Computer computer(fileName);
+  Computer computer(fileName, false);
 
   computer.startUp();
 
@@ -13,7 +13,7 @@ int partOne(const std::string fileName) {
 int partTwo(const std::string fileName) {
   std::cout << "Part 2: " << fileName << std::endl;
 
-  Computer computer(fileName);
+  Computer computer(fileName, false);
 
   computer.startUp();
 
@@ -42,39 +42,39 @@ int main(int argc, char **argv) {
   /// EXAMPLE 0 PART 2
   std::cout << "Testing equal to in position mode. Input an 8... " << std::endl;
   std::vector<signed int> exampleEqualPosition = {3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8};
-  Computer testComputerEqualPosition(exampleEqualPosition);
+  Computer testComputerEqualPosition(exampleEqualPosition, false);
   testComputerEqualPosition.startUp();
   std::cout << "If you saw a 1, the test was a success." << std::endl;
 
   /// EXAMPLE 1 PART 2
   std::cout << "Testing less than in position mode. Input a value less than 8... " << std::endl;
   std::vector<signed int> exampleLessThanPosition = {3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8};
-  Computer testComputerLessThanPosition(exampleLessThanPosition);
+  Computer testComputerLessThanPosition(exampleLessThanPosition, false);
   testComputerLessThanPosition.startUp();
   std::cout << "If you saw a 1, the test was a success." << std::endl;
 
   /// EXAMPLE 2 PART 2
   std::cout << "Testing equal to in immediate mode. Input an 8... " << std::endl;
   std::vector<signed int> exampleEqualImmediate = {3, 3, 1108, -1, 8, 3, 4, 3, 99};
-  Computer testComputerEqualImmediate(exampleEqualImmediate);
+  Computer testComputerEqualImmediate(exampleEqualImmediate, false);
   testComputerEqualImmediate.startUp();
   std::cout << "If you saw a 1, the test was a success." << std::endl;
 
   /// EXAMPLE 3 PART 2
   std::cout << "Testing less than in immediate mode. Input a value less than 8... " << std::endl;
   std::vector<signed int> exampleLessThanImmediate = {3, 3, 1107, -1, 8, 3, 4, 3, 99};
-  Computer testComputerLessThanImmediate(exampleLessThanImmediate);
+  Computer testComputerLessThanImmediate(exampleLessThanImmediate, false);
   testComputerLessThanImmediate.startUp();
   std::cout << "If you saw a 1, the test was a success." << std::endl;
 
   std::cout << "Testing jump in position mode. Expect a 1 if the value is nonzero or a 0 if the value is 0..." << std::endl;
   std::vector<signed int> exampleJumpPosition = {3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9};
-  Computer testComputerJumpPosition(exampleJumpPosition);
+  Computer testComputerJumpPosition(exampleJumpPosition, false);
   testComputerJumpPosition.startUp();
 
   std::cout << "Testing jump in immediate mode. Expect a 1 if the value is nonzero or a 0 if the value is 0..." << std::endl;
   std::vector<signed int> exampleJumpImmediate = {3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1};
-  Computer testComputerJumpImmediate(exampleJumpImmediate);
+  Computer testComputerJumpImmediate(exampleJumpImmediate, false);
   testComputerJumpImmediate.startUp();
 
   int partTwoResult = partTwo("input.txt");
